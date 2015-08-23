f8d148a736cb540b3c0d6bd01dba0f0217aae11b
W. Trevor King <wking@tremily.us>
2015-08-23 11:28:41 -0700

    Reformat with indent
    GNU-style seemed the closest match to the existing codebase, so I
    used:
    
      $ indent --ignore-profile --gnu-style *.c *.h
    
    With GNU indent 2.2.11.
    
    This fixes a lot of inconsistent tab-vs.-space indenting (among other
    things).
    
    [1]: http://www.gnu.org/software/indent/

--- id3read.h
+++ id3read.h
@@ -1,13 +1,13 @@
 #ifndef ID3HEADER_INCLUDED
 #define ID3HEADER_INCLUDED
 
-gchar * getArtist (struct id3_tag *tag);
-gchar * getAlbum(struct id3_tag *tag);
-gchar * getYear(struct id3_tag *tag);
-gchar * getTitle(struct id3_tag *tag);
-gchar * getGenre(struct id3_tag *tag);
+gchar *getArtist (struct id3_tag * tag);
+gchar *getAlbum (struct id3_tag *tag);
+gchar *getYear (struct id3_tag *tag);
+gchar *getTitle (struct id3_tag *tag);
+gchar *getGenre (struct id3_tag *tag);
 int getSonglen (struct id3_tag *tag);
-gchar * getTracknum (struct id3_tag *tag);
-gchar * getOrigFilename (struct id3_tag *tag);
+gchar *getTracknum (struct id3_tag *tag);
+gchar *getOrigFilename (struct id3_tag *tag);
 
 #endif
